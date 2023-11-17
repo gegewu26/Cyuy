4f8d66fde496c2583edc02dd8272a25a3027abe9
月见花 <149349310+gegewu26@users.noreply.github.com>
2023-11-17 14:44:08 +0800

    Add files via upload

---
+++ 源.cpp
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	int arr1[4] = { 1,2,3,4 };
+//	int arr2[4] = { 2,3,4,5 };
+//	int arr3[4] = { 3,4,5,6 };
+//	int arr[4];
+//	for (int x = 0; x < 4; x++)
+//	{
+//		printf("%d %d %d %d ", arr1[x], arr2[x], arr3[x], arr[x]);
+//		printf("\n");
+//	}
+//	
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//// 定义一个结构体类型
+//struct Person {
+//    char name[20];
+//    int age;
+//    float height;
+//};
+//
+//int main() {
+//    // 声明一个结构体变量
+//    struct Person person1;
+//
+//    // 访问结构体成员并赋值
+//    strcpy(person1.name, "John");
+//    person1.age = 25;
+//    person1.height = 1.75;
+//
+//    // 打印结构体成员的值
+//    printf("Name: %s\n", person1.name);
+//    printf("Age: %d\n", person1.age);
+//    printf("Height: %.2f\n", person1.height);
+//
+//    return 0;
+//}
+//int
+//#include <stdio.h>
+//
+//int countSetBits(int n,int m) 
+//{
+//    int a = (n^m);
+//    int count = 0;
+//    while (a!=0)
+//    {
+//        if (a & 1);
+//        {
+//            count++;
+//        }
+//        a = a>> 1;
+//    }
+//    return count;
+//}
+//
+//int main() 
+//{
+//    int n = 0;
+//    int m = 0;
+//    scanf("%d %d", &n, &m);
+//    int result = countSetBits(n, m);
+//    printf("%d\n",result);
+//    return 0;
+//}
