3a11fbd9ddf04ab8e96e5d6e0f68311114d0bbb9
月见花 <149349310+gegewu26@users.noreply.github.com>
2023-11-21 23:31:14 +0800

    Add files via upload

---
+++ deta11-21.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//
+//int fibonacci(int n) {
+//    if (n <= 1) {
+//        return n;
+//    }                                             //(电脑运行方式)
+//    return fibonacci(n - 1) + fibonacci(n - 2);//计算前两个数之和，递归得到的数大于等于1时继续递归
+//                                               //直到全部数小于1，把得到的数进行相加
+//}                                              
+//                                                  //(第二种理解方式)
+//                                               // 前面两数相加   
+//                                                
+//
+//int main() {
+//    int n = 40; 
+//    printf("斐波那契数列前%d个数为：\n",n);
+//  for (int i = 1; i < n; i++) {
+//        printf("%d ", fibonacci(i));
+//   } 
+//    return 0;
+//}
+
+
+int main()
+{
+	system("shutdown /s/t");
+	return 0;
+}
