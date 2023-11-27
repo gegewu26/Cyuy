dabcc8651741f9566cde4b4dad6b12d918e7074f
月见花 <149349310+gegewu26@users.noreply.github.com>
2023-11-27 21:58:56 +0800

    Add files via upload

---
+++ test11-27.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 10;
+//	scanf("%d", &a);
+//		while (b--)
+//		{
+//			a %= 10;
+//			if (a %= 2)
+//			{
+//				printf("%d", a);
+//			}
+//		}
+//	return 0;
+//}
+
+
+
+//int main() {
+//    int arr[10];
+//    memset(arr, 0, sizeof(arr));
+//
+//    // 打印数组的值
+//    for (int i = 0; i < 10; i++) {
+//        printf("%d ", arr[i]);
+//    }
+//
+//    return 0;
+//}
+//
+//int main() {
+//    int arr[10];
+//    memset(arr, 1, sizeof(arr));
+//
+//    // 打印数组的值
+//    for (int i = 0; i < 10; i++) {
+//        printf("%d ", arr[i]);
+//    }
+//
+//    return 0;
+//}
