79ed440b75274cbdfbded3c6c4b755d5a8ac2de6
月见花 <149349310+gegewu26@users.noreply.github.com>
2023-11-28 21:21:25 +0800

    Add files via upload

---
+++ test11-28.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int j = 1;
+//	for (int i = 1; i < 10; i++)
+//	{
+//		j = (++j) * 2;
+//	}
+//	printf("%d", j);
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	scanf("%d", &i);
+//	for (j = 0; j < i; j++)
+//	{
+//		for (j = i-1; j >0; j--)
+//		{
+//			char j = ' ';
+//			printf("%c\n", j);
+//		}
+//		for (j = 0; j < i - 1; j++)
+//		{
+//			char j = '*';
+//			printf("%c", j);
+//		}
+//	}
+//	for (j = 0; j < i - 1; j++)
+//	{
+//
+//	}
+//
+//	return 0;
+//}
