95fae357d553f594c1868eac3c7288b148cf850b
月见花 <149349310+gegewu26@users.noreply.github.com>
2023-11-29 22:07:11 +0800

    Add files via upload

---
+++ test11-29.c
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	int i, j, n;
+//	printf("请输入需要打印的列数>:");
+//	scanf("%d", &n);
+//	j = n;
+//	while (n)
+//	{
+//		for (i = 0; i < n - 1; i++)
+//		{
+//			printf(" ");
+//		}
+//
+//		for (i = j; i >n - 1; i--)
+//		{
+//			printf("*");
+//		}
+//		n--;
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	float i = 2.00;
+//	float j = 1.00;
+//	float num = 0.00;
+//	int b = j;
+//	for (int a = 0; a < 20; a++)
+//	{
+//		num += i / j;
+//		j = i;
+//		i = i + b;	
+//		b = j;
+//	}
+//	printf("%0.2f", num);
+//	return 0;
+//}
+//char dxu(char* a)
+//{
+//
+//}
+//int main()
+//{
+//	char a[5] = "";
+//	scanf("%s", a);
+//	char b= dxu(&a);
+//	for (int c = 0; c < 5; c++)
+//	{
+//		printf("%c", b);
+//	}
+//	return 0;
+//}
